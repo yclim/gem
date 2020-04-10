ece02a0f341ebbb83cb2a9bac2c3b20eef993cca
neojunjie <neojunjie87@gmail.com>
2020-04-10 23:57:06 +0800

    -- extract basic information from file

---
+++ src/main/java/innohack/gem/entity/DocumentMetadata.java
@@ -0,0 +1,87 @@
+package innohack.gem.entity;
+
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Date;
+
+public class DocumentMetadata {
+    private String name;
+    private String contentType;
+    private Long size;
+    private String extension;
+    private Date createDate;
+    private String filePath;
+
+    public DocumentMetadata() {
+    }
+
+    public DocumentMetadata(MultipartFile file) {
+        this.name = file.getOriginalFilename();
+        this.size = file.getSize();
+        this.extension = FilenameUtils.getExtension(file.getOriginalFilename());
+        this.createDate = new Date();
+        this.contentType = file.getContentType();
+        // todo: use tika to get mimetype
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public Long getSize() {
+        return size;
+    }
+
+    public void setSize(Long size) {
+        this.size = size;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    @Override
+    public String toString() {
+        return "DocumentMetadata{\n" +
+                "name='" + name + '\'' + "\n" +
+                ", contentType='" + contentType + '\'' + "\n" +
+                ", size=" + size + "\n" +
+                ", extension='" + extension + '\'' + "\n" +
+                ", createDate=" + createDate + "\n" +
+                ", filePath='" + filePath + '\'' + "\n" +
+                '}';
+    }
+}
