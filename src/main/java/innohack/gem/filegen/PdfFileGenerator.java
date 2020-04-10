240b82f2ade2fa45ae43c4d3327f47c26dd130dd
yowcheng <yowcheng.lim@gmail.com>
2020-04-10 10:48:57 +0800

    touch up on PDfFileGenerator

--- src/main/java/innohack/gem/filegen/PdfFileGenerator.java
+++ src/main/java/innohack/gem/filegen/PdfFileGenerator.java
@@ -6,7 +6,6 @@ import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -17,10 +16,10 @@ import java.util.List;
  * Code to generate PDF documents.
  * PDF contains a header text and a list of records. that looks like this:
  *
- * PRODUCT INFORMATION :: PRODUCT_ID, PRODUCT_NAME, QUANTITY
- * p381356, product_381356, 3813560
- * p381357, product_381357, 3813570
- * p381358, product_381358, 3813580
+ * PRODUCT INFORMATION :: PRODUCT_ID, PRODUCT_NAME, PRICE, QUANTITY
+ * p81035397, product_ulqcboznxu, 7839.24, 689
+ * p44218053, product_jsppfrleqq, 7900.89, 54
+ * p89301633, product_gjjuohwkwp, 754.96, 904
  */
 public class PdfFileGenerator {
 
@@ -48,31 +47,31 @@ public class PdfFileGenerator {
         document.close();
     }
 
-    public static List<List<String>> generateDataTable(int start, int end) {
+    public static List<List<String>> generateDataTable(int rows) {
         List<List<String>> list = new ArrayList<>();
-        for (int i = start; i < end; i++) {
-            list.add(Arrays.asList("p" + i, "product_" + GenUtil.randomString(10), String.valueOf(i * 10)));
+        for (int i = 0; i < rows; i++) {
+            String id = String.format("p%08d",GenUtil.randomInt(99999999));
+            String product = "product_" + GenUtil.randomString(10);
+            String price = GenUtil.randomInt(9999) + "." + String.format("%02d", GenUtil.randomInt(99));
+            String quantity = String.valueOf(GenUtil.randomInt(999));
+            list.add(Arrays.asList(id, product, price, quantity));
         }
         return list;
     }
 
     public static void generateProductsPdfFiles(int numOfFiles, Path dest) throws IOException {
-        String HEADER_TEXT = "PRODUCT INFORMATION :: PRODUCT_ID, PRODUCT_NAME, QUANTITY";
+        String HEADER_TEXT = "PRODUCT INFORMATION :: PRODUCT_ID, PRODUCT_NAME, PRICE, QUANTITY";
         String fileNamePrefix = "product_info_";
         List<String> textLines = null;
 
         for (int i = 0; i < numOfFiles; i++) {
             textLines = new ArrayList<>();
             textLines.add(HEADER_TEXT);
-            int startOffset = GenUtil.randomInt(1000000);
             int numOfRecords = GenUtil.randomInt(15);
             String filename = fileNamePrefix + "_" + i + ".pdf";
-            List<List<String>> dataTable = generateDataTable(startOffset, startOffset + numOfRecords);
+            List<List<String>> dataTable = generateDataTable(numOfRecords);
             for (List<String> row: dataTable) {
                 textLines.add(String.join(", ", row));
-            }
-            if (!dest.toFile().exists()) {
-                Files.createDirectory(dest);
             }
             generateTextualPdfFiles(Paths.get(dest.toString(), filename), textLines);
         }
