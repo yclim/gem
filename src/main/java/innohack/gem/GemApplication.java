ff0b89fd6e89d199bc8122db5d1e22491a397983
onegang <leethiamchye@gmail.com>
2020-04-27 18:00:35 +0800

    Complete a CD pipeline

--- src/main/java/innohack/gem/GemApplication.java
+++ src/main/java/innohack/gem/GemApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GemApplication { 
 
   public static void main(String[] args) {
-    System.out.println("test");
     SpringApplication.run(GemApplication.class, args);
   }
 }
