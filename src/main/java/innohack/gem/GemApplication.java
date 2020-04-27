bab3121ca65d2d90c10ef5f7f6989a8be4a5aba1
onegang <leethiamchye@gmail.com>
2020-04-27 16:18:53 +0800

    Try full dockerize build

--- src/main/java/innohack/gem/GemApplication.java
+++ src/main/java/innohack/gem/GemApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GemApplication {
+public class GemApplication { 
 
   public static void main(String[] args) {
     SpringApplication.run(GemApplication.class, args);
