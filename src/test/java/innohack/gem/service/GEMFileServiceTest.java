00d19bba363c32ef58a3bbfa21597a043955d205
Shao Jie <shaojieyew@gmail.com>
2020-04-28 13:34:22 +0800

    Fix testcase

--- src/test/java/innohack/gem/service/GEMFileServiceTest.java
+++ src/test/java/innohack/gem/service/GEMFileServiceTest.java
@@ -1,5 +1,6 @@
 package innohack.gem.service;
 
+import com.google.common.collect.Lists;
 import innohack.gem.dao.GEMFileDao;
 import innohack.gem.dao.GroupDao;
 import innohack.gem.entity.GEMFile;
@@ -24,26 +25,42 @@ public class GEMFileServiceTest {
   GEMFile txtFile = new GEMFile("dump.txt", "src/test/resources");
   GEMFile datFile = new GEMFile("data.dat", "src/test/resources");
 
+  private List<File> getFiles(String path){
+    File dir = new File(path);
+    List<File> resultList = Lists.newArrayList();
+    File[] fList = dir.listFiles();
+    if (fList != null) {
+      for (File file : fList) {
+        if (file.isFile()) {
+          resultList.add(file);
+        } else {
+          resultList.addAll(getFiles(file.getAbsolutePath()));
+        }
+      }
+    }
+    return resultList;
+  }
+
   @Test
   public void testSync() throws Exception {
 
     // gen file
-    String[] genMockParam = {"target/samples/", "5"};
+    String dir = "target/samples/";
+    String[] genMockParam = {dir, "5"};
     GenerateMockFiles.main(genMockParam);
-    File folder = new File("target/samples/");
     // sync file, test synced file count
     List<GEMFile> list1 = gemFileService.syncFiles("target/samples/");
-    assert (folder.listFiles().length == list1.size());
+    assert (getFiles(dir).size() == list1.size());
 
     // delete 3 files
     int deleteFile = 3;
-    for (File f : folder.listFiles()) {
+    for (File f : getFiles(dir)) {
       f.delete();
       deleteFile--;
       if (deleteFile == 0) break;
     }
     // sync file again and test file count again
     List<GEMFile> list = gemFileService.syncFiles("target/samples/");
-    assert (list.size() == new File("target/samples/").listFiles().length);
+    assert (list.size() == getFiles(dir).size());
   }
 }
