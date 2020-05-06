db412d2837fcd68f9a8b704ea6d618275e0b3e57
yowcheng <yowcheng.lim@gmail.com>
2020-05-06 11:22:26 +0800

    add test files

--- src/test/java/innohack/gem/entity/rule/rules/TikaMetadataValueTest.java
+++ src/test/java/innohack/gem/entity/rule/rules/TikaMetadataValueTest.java
@@ -10,7 +10,8 @@ public class TikaMetadataValueTest {
 
   @Test
   public void testValidCheckTikaMetadata() throws Exception {
-    GEMFile pdfFile = new GEMFile("story_0.pdf", "src/test/resources/rules");
+    GEMFile pdfFile =
+        new GEMFile("story_0.pdf", "src/test/resources/innohack/gem/entity/rule/rules");
     pdfFile.extract();
     TikaMetaValue metadaCheck = new TikaMetaValue("Content-Type", "application/pdf");
     assertTrue(metadaCheck.check(pdfFile));
@@ -18,7 +19,8 @@ public class TikaMetadataValueTest {
 
   @Test
   public void testInvalidCheckTikaMetadata() throws Exception {
-    GEMFile pdfFile = new GEMFile("story_0.pdf", "src/test/resources/rules");
+    GEMFile pdfFile =
+        new GEMFile("story_0.pdf", "src/test/resources/innohack/gem/entity/rule/rules");
     pdfFile.extract();
     TikaMetaValue metadaCheck = new TikaMetaValue("Owner", "orhorh");
     assertFalse(metadaCheck.check(pdfFile));
