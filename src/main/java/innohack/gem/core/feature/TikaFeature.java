74e8930d1753037a3da46714abcdde788b0b453f
tay.ct <duotayct@gmail.com>
2020-11-19 09:55:18 +0800

    added in bomremover

--- src/main/java/innohack/gem/core/feature/TikaFeature.java
+++ src/main/java/innohack/gem/core/feature/TikaFeature.java
@@ -1,7 +1,12 @@
 package innohack.gem.core.feature;
 
-import java.io.File;
-import java.io.FileInputStream;
+import java.io.*;
+import java.nio.charset.Charset;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.input.BOMInputStream;
+import org.apache.tika.config.TikaConfig;
+import org.apache.tika.detect.EncodingDetector;
+import org.apache.tika.io.TikaInputStream;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.AutoDetectParser;
 import org.apache.tika.parser.ParseContext;
@@ -12,6 +17,16 @@ import org.apache.tika.sax.BodyContentHandler;
 public class TikaFeature extends AbstractFeature {
 
   private String content;
+
+  private static TikaConfig tikaConfig;
+
+  public static TikaConfig getTikaConfig() throws Exception {
+    if (tikaConfig == null) {
+      tikaConfig = new TikaConfig();
+    }
+
+    return tikaConfig;
+  }
 
   public String getContent() {
     return content;
@@ -25,16 +40,46 @@ public class TikaFeature extends AbstractFeature {
   public void extract(File f) throws Exception {
     Parser parser = new AutoDetectParser();
     Metadata metadata = new Metadata();
+
     try (FileInputStream inputStream = new FileInputStream(f)) {
       ParseContext context = new ParseContext();
+
       BodyContentHandler handler = new BodyContentHandler(-1);
       parser.parse(inputStream, handler, metadata, context);
-      this.content = handler.toString();
+
+      this.content = removeByteOrder(handler.toString(), detectEncoding(f));
     }
 
     String[] metadataNames = metadata.names();
     for (String name : metadataNames) {
       addMetadata(name, metadata.get(name));
+    }
+  }
+
+  private Charset detectEncoding(File f) throws Exception {
+
+    InputStream targetStream = new FileInputStream(f);
+    EncodingDetector encodeDetector = this.getTikaConfig().getEncodingDetector();
+
+    Metadata metadata = new Metadata();
+    metadata.set(Metadata.RESOURCE_NAME_KEY, f.getAbsolutePath());
+
+    return encodeDetector.detect(TikaInputStream.get(targetStream), metadata);
+  }
+
+  private String removeByteOrder(String record, Charset charset) throws Exception {
+    byte[] bytes = record.getBytes(charset);
+
+    InputStream inputStream = new ByteArrayInputStream(bytes);
+
+    BOMInputStream bomInput = new BOMInputStream(inputStream, false);
+
+    if (bomInput.hasBOM()) {
+      String rtnResult = IOUtils.toString(bomInput, charset);
+
+      return rtnResult;
+    } else {
+      return record;
     }
   }
 }
