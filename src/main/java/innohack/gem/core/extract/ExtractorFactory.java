bcf34bdb0fb92c751aad429ca49b9fd186c4df01
lee.tc <lee.tc@host.docker.internal>
2021-02-22 17:18:11 +0800

    Fix to allow JAR file to run

--- src/main/java/innohack/gem/core/extract/ExtractorFactory.java
+++ src/main/java/innohack/gem/core/extract/ExtractorFactory.java
@@ -1,12 +1,15 @@
 package innohack.gem.core.extract;
 
+import com.beust.jcommander.internal.Sets;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
-import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.core.type.filter.AssignableTypeFilter;
 
 public class ExtractorFactory {
 
@@ -24,10 +27,25 @@ public class ExtractorFactory {
     return extractors;
   }
 
-  private static Iterator<Class<? extends AbstractExtractor>> getAllConcreteExtractorClass() {
-    Reflections reflections = new Reflections(AbstractExtractor.class.getPackage().getName());
-    Set<Class<? extends AbstractExtractor>> allClasses =
-        reflections.getSubTypesOf(AbstractExtractor.class);
-    return allClasses.iterator();
+  private static Iterator<Class<? extends AbstractExtractor>> getAllConcreteExtractorClass()
+      throws ClassNotFoundException {
+    Set<Class<? extends AbstractExtractor>> results = Sets.newHashSet();
+    ClassPathScanningCandidateComponentProvider provider =
+        new ClassPathScanningCandidateComponentProvider(false);
+    provider.addIncludeFilter(new AssignableTypeFilter(AbstractExtractor.class));
+
+    // scan in org.example.package
+    Set<BeanDefinition> components =
+        provider.findCandidateComponents(AbstractExtractor.class.getPackage().getName());
+    for (BeanDefinition component : components) {
+      Class cls = Class.forName(component.getBeanClassName());
+      results.add(cls);
+    }
+    return results.iterator();
+    //
+    //    Reflections reflections = new Reflections(AbstractExtractor.class.getPackage().getName());
+    //    Set<Class<? extends AbstractExtractor>> allClasses =
+    //        reflections.getSubTypesOf(AbstractExtractor.class);
+    //    return allClasses.iterator();
   }
 }
